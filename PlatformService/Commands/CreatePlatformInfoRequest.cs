f7761fde59592861513b341be427d2e4f21b4a47
AliAlhadi <alialhadiabokhalil@gmail.com>
2024-08-10 13:48:51 +0300

    Resolve merge conflicts

---
+++ PlatformService/Commands/CreatePlatformInfoRequest.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using PlatformService.Dto_s;
+
+namespace PlatformService.Commands
+{
+	public class CreatePlatformInfoRequest : IRequest<PlatformReadDto>
+	{
+		public PlatformCreateDto CreatePlatform { get; set; }
+
+        public CreatePlatformInfoRequest(PlatformCreateDto CreatePlatformDTO)
+        {
+			CreatePlatform = CreatePlatformDTO;
+
+		}
+    }
+}
